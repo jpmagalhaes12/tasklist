1f56b8be71a06d92e54f542354834586baa0462a
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-11-25 17:00:05 -0300

    criando model e carregando ele

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ ok: true }));
+routes.get('/teste', async (req, res) => {
+    const user = await User.create({
+        name: 'joão',
+        email: 'joao@joao.com',
+        password_hash: '123123',
+    });
+    return res.json(user);
+});
 
 export default routes;
