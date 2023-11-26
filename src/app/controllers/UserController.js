7308dd91a2a8c9a74f00511261111bb60408c148
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-11-26 00:27:26 -0300

    autenticação do usuário para update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,6 +16,11 @@ class UserController {
             name,
         });
     }
+
+    async update(req, res) {
+        console.log(req.userId);
+        return res.json({ ok: true });
+    }
 }
 
 export default new UserController();
